ef4dba41f619263c7d73b405dd6cfd826b432954
tiagrc <tiago.rodrigues.tr99@hotmail.com>
2025-07-30 22:46:18 -0300

    HUB FORUM ALURA

---
+++ src/main/java/tiago/HubForum/HubForumApplication.java
@@ -0,0 +1,13 @@
+package tiago.HubForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tiago/HubForum/HubForumApplicationTests.java
@@ -0,0 +1,13 @@
+package tiago.HubForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
